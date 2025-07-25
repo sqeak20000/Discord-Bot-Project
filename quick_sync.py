81b0d81fe38c9e3550f9e6a6b69b141fb0f560d1
sqeak20000 <astegora@gmail.com>
2025-07-24 21:28:09 -0500

    updated guild

--- quick_sync.py
+++ quick_sync.py
@@ -16,7 +16,7 @@ from config import BOT_TOKEN
 from moderation import setup_moderation_commands
 
 # REPLACE THIS WITH YOUR ACTUAL DISCORD SERVER ID
-YOUR_GUILD_ID = 123456789012345678  # <-- CHANGE THIS!
+YOUR_GUILD_ID = 1017032575354683433  # <-- CHANGE THIS!
 
 async def quick_sync():
     """Sync slash commands to a specific guild for immediate testing"""
